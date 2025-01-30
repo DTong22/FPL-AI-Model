eea95ae9f5f3149e518e96104a2f676dcdec75c0
DTong22 <115417874+DTong22@users.noreply.github.com>
2025-01-30 11:09:16 +0000

    Update optimization.py

--- optimization.py
+++ optimization.py
@@ -1,11 +1,11 @@
 import pandas as pd
 from pulp import LpProblem, LpVariable, lpSum, LpMaximize, LpStatus
 
-def dream_team(df_pivot, n_next_rounds, fixtures):
+def dream_team(df_pivot, n_next_rounds, fixtures, budget_limit=84):
     df = df_pivot[df_pivot['status']==100]   #100% players only
     selected = LpVariable.dicts("selected", df.index, cat="Binary")
 
-    # Step 3: Define the objective function
+    # Define the objective function
     model = LpProblem("FPL_Team_Optimization", LpMaximize)
     captain_factor = 2  # Factor to double the captain's points
 
@@ -23,11 +23,9 @@ def dream_team(df_pivot, n_next_rounds, fixtures):
     captain_points = df[col].max()  # Get the maximum points among all players
     model += objective + captain_factor * captain_points * lpSum(selected[i] for i in df.index)  # Add captain's points
 
-    # Step 4: Set the budget constraint
-    budget_limit = 200#float(input('enter team value:'))-16  # Set your budget limit here
     model += lpSum(df["value"][i] * selected[i] for i in df.index) <= budget_limit
 
-    # Step 5: Set the position requirements constraint
+    # Set the position requirements constraint
     position_constraints = {"GK": 1, "DEF": 3,"MID": 5, "FWD": 2 }  # Set your position requirements here
     for position, requirement in position_constraints.items():
         model += lpSum(selected[i] for i in df.index if df["position"][i] == position) == requirement
@@ -37,16 +35,16 @@ def dream_team(df_pivot, n_next_rounds, fixtures):
         for team in teams:
             model += lpSum(selected[i] for i in df.index if df["team"][i] == team and df["position"][i] == position) <= 2
 
-    # Step 6: Set the constraint on the maximum number of players from a single team
+    # Set the constraint on the maximum number of players from a single team
     max_players_per_team = 3  # Set the maximum number of players from a single team here
     teams = df["team"].unique().tolist()
     for team in teams:
         model += lpSum(selected[i] for i in df.index if df["team"][i] == team) <= max_players_per_team
 
-    # Step 7: Solve the linear programming problem
+    # Solve the linear programming problem
     model.solve()
 
-    # Step 8: Print the selected players, their positions, and expected points
+    # Print the selected players, their positions, and expected points
     print("Status:", LpStatus[model.status])
     selected_players = [i for i in df.index if selected[i].value() == 1]
     selected_data = df.loc[selected_players, ["name", "position", "value", "team", col]]
